c90038bd051ae63b47600fa8748058c7849742ad
Valar <Valar@npcompete.net>
2017-06-03 16:24:37 +0530

    First template added

--- dnsQuery/urls.py
+++ dnsQuery/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from dnsQuery import views
 
 urlpatterns = [
-    url(r'^', views.home),
+    url(r'^', views.index, name='queryList'),
+    url(r'^author', views.home),
 ]
