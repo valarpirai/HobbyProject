1f71ffaaabe12ee41ec98fe55266d7b466b79fc6
Valarpirai <a.valarpirai@gmail.com>
2017-11-02 19:22:39 +0530

    Deleted old django files

--- dnsQuery/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DnsqueryConfig(AppConfig):
-    name = 'dnsQuery'
--- dnsQuery/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from dnsQuery import views
-
-urlpatterns = [
-    url(r'^', views.index, name='queryList'),
-    url(r'^author', views.home),
-]
